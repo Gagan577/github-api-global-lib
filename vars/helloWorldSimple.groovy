695061954b09103d75ea1710ecac09cba77963b1
Gagan577 <88342501+Gagan577@users.noreply.github.com>
2022-04-29 11:11:30 +0530

    Update helloWorldSimple.groovy

--- vars/helloWorldSimple.groovy
+++ vars/helloWorldSimple.groovy
@@ -1,28 +1,7 @@
 def call(String name, String dayOfWeek) {
   emailext body: '''<!DOCTYPE html>
-<html>
-<head>
-<style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
 
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-</style>
-</head>
-<body>
-
-<h2>HTML Table</h2>
 
 <table>
   <tr>
@@ -31,17 +10,17 @@ tr:nth-child(even) {
     <th>$BUILD_NUMBER</th>
   </tr>
   <tr>
-    <td>$PROJECT_NAM</td>
-    <td>$PROJECT_NAM</td>
-    <td>$PROJECT_NAM</td>
+    <td>$PROJECT_NAME</td>
+    <td>$PROJECT_NAME</td>
+    <td>$PROJECT_NAME</td>
   </tr>
   <tr>
-    <td>$PROJECT_NAM Moctezuma</td>
+    <td>$PROJECT_NAME Moctezuma</td>
     <td>$PROJECT_NAM Chang</td>
     <td>$PROJECT_NAM</td>
   </tr>
   <tr>
-    <td>$PROJECT_NAM</td>
+    <td>$PROJECT_NAME</td>
     <td>Roland Mendel</td>
     <td>Austria</td>
   </tr>
