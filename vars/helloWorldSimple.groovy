d6881c276989f757e14cbd26c05544aed9fa3633
Gagan577 <88342501+Gagan577@users.noreply.github.com>
2022-05-04 22:09:35 +0530

    Update helloWorldSimple.groovy

--- vars/helloWorldSimple.groovy
+++ vars/helloWorldSimple.groovy
@@ -1,3 +1,3 @@
 def call(String name, String dayOfWeek) {
-  emailext body: '${name}', subject: '${dayOfWeek}', to: '97c80a7e7e-d118f8@inbox.mailtrap.io'
+  emailext body: '${name}', subject: '${name}', to: '97c80a7e7e-d118f8@inbox.mailtrap.io'
 }
